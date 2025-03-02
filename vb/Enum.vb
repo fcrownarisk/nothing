256f0d8acac9d9aae6fdfbab2057cca465282943
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-02 20:26:34 +0800

    Add files via upload

---
+++ vb/Enum.vb
@@ -0,0 +1,36 @@
+Dim Vertex As V
+Dim Edge As E
+Dim Face As F
+Function Euler
+ V - E + F = 2 
+End Function Euler
+
+enum Tetrahedron{
+V = 4
+E = 6
+F = 4
+}
+
+enum Hexahedron{
+V = 8
+E = 12
+F = 6
+}
+
+enum Octahedron{
+V = 6
+E = 12
+F = 8
+}
+
+enum Dodecahedron{
+V = 20
+E = 30
+F = 12
+}
+
+enum Icosahedron{
+V = 12
+E = 30
+F = 20
+}
